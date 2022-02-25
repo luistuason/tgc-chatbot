0a5a2d7648ab8309a253f75c08e775422a28a8d9
Luis Tuason <philipluistuason@gmail.com>
2022-02-25 23:26:17 +0800

    fixed typo in declaring modules

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const
     express = require('express'),
     bodyParser = require('body-parser'),
-    request = require('request');
+    request = require('request'),
     app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
